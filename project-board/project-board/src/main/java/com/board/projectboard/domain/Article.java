c302f26c00f57fc6d28ce8760c6f4f49a3019b8e
JEONGBEOM KO <beom9807@gmail.com>
2022-09-19 22:18:56 +0900

    #9

--- project-board/project-board/src/main/java/com/board/projectboard/domain/Article.java
+++ project-board/project-board/src/main/java/com/board/projectboard/domain/Article.java
@@ -1,12 +1,31 @@
 package com.board.projectboard.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Getter
+@ToString
+@Table(indexes = {
+
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy"),
+})
+@Entity
 public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String title; // 제목
-    private String content; // 내용
-    private String hashtag; //해시태그
+
+    @Setter private String title; // 제목
+    @Setter private String content; // 내용
+
+    @Setter private String hashtag; //해시태그
 
     private LocalDateTime createdAt; //생성일시
     private String createdBy; //생성자
