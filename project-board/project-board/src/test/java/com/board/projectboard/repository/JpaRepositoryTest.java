d1c9d42274f2d113aba01d4ea2c77715819a5b31
JEONGBEOM KO <beom9807@gmail.com>
2022-09-20 00:14:36 +0900

    #10 - repository interface 작성
    이걸로 db에 접근

--- project-board/project-board/src/test/java/com/board/projectboard/repository/JpaRepositoryTest.java
+++ project-board/project-board/src/test/java/com/board/projectboard/repository/JpaRepositoryTest.java
@@ -37,6 +37,50 @@ class JpaRepositoryTest {
         //Then
         assertThat(articles)
                 .isNotNull()
-                .hasSize(0);
+                .hasSize(123);
+    }
+    @DisplayName("insert 테스트")
+    @Test
+    void givenTestData_whenInserting_thenWorksFine(){
+
+        //Given
+        long previousCount = articleRepository.count();
+        //When
+        Article savedArticle = articleRepository.save(Article.of("new article", "new content", "#spring"));
+        //Then
+        assertThat(articleRepository.count()).isEqualTo(previousCount + 1);
+
+    }
+    @DisplayName("update 테스트")
+    @Test
+    void givenTestData_whenUpdating_thenWorksFine(){
+
+        //Given
+        Article article = articleRepository.findById(1L).orElseThrow();
+        String updateHashtag = "#springboot";
+        article.setHashtag(updateHashtag);
+
+        //When
+        Article savedArticle = articleRepository.saveAndFlush(article);
+
+        //Then
+        assertThat(savedArticle).hasFieldOrPropertyWithValue("hashtag", updateHashtag);
+    }
+    @DisplayName("delete 테스트")
+    @Test
+    void givenTestData_whenDeleting_thenWorksFine(){
+
+        //Given
+        Article article = articleRepository.findById(1L).orElseThrow();
+        long previousArticleCount = articleRepository.count();
+        long previousArticleCommentCount = articleCommentRepository.count();
+        int deletedCommentSize = article.getArticleComments().size();
+
+        //When
+        articleRepository.delete(article);
+
+        //Then
+        assertThat(articleRepository.count()).isEqualTo(previousArticleCount -1);
+        assertThat(articleCommentRepository.count()).isEqualTo(previousArticleCommentCount - deletedCommentSize);
     }
 }
