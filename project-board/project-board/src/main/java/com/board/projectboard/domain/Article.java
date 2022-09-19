e2e97912f3be3424a6e0bb1aef5e4b28026689ec
JEONGBEOM KO <beom9807@gmail.com>
2022-09-19 23:25:49 +0900

    #12
    데이터베이스 접근 로직

--- project-board/project-board/src/main/java/com/board/projectboard/domain/Article.java
+++ project-board/project-board/src/main/java/com/board/projectboard/domain/Article.java
@@ -10,7 +10,9 @@ import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Getter
 @ToString
@@ -32,10 +34,17 @@ public class Article {
 
     @Setter private String hashtag; //해시태그
 
+    @ToString.Exclude
+    @OrderBy("id")
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
+
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
+
     @CreatedDate @Column(nullable = false) private LocalDateTime createdAt; //생성일시
     @CreatedBy @Column(nullable = false, length = 100) private String createdBy; //생성자
     @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt; //수정일시
     @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy; //수정자
+
 
     protected Article() {
     }
