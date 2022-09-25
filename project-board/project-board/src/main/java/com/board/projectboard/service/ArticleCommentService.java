2af4242af7a1e1bf4bf1c366b75442e5d97fb868
JEONGBEOM KO <beom9807@gmail.com>
2022-09-25 22:10:06 +0900

    #22 - 게시판 뷰 템플릿 구현
    그 밖에 헤더, 푸터 목업 표현을 추가 (서버 렌더링을 안 하고 html을 그냥 덮어도 맥락을 파악 할 수 있도록 함

--- project-board/project-board/src/main/java/com/board/projectboard/service/ArticleCommentService.java
+++ project-board/project-board/src/main/java/com/board/projectboard/service/ArticleCommentService.java
@@ -1,9 +1,12 @@
 package com.board.projectboard.service;
 
 import com.board.projectboard.dto.ArticleCommentDto;
+import com.board.projectboard.dto.ArticleDto;
 import com.board.projectboard.repository.ArticleCommentRepository;
 import com.board.projectboard.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -34,4 +37,5 @@ public class ArticleCommentService {
 
     public void deleteArticleComment(Long articleCommentId, String userId) {
     }
+
 }
