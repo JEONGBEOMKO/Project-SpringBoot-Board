7e70254d07b1005bb652f81005e0bf826c8edf47
JEONGBEOM KO <beom9807@gmail.com>
2022-09-26 07:18:52 +0900

    #26 - 페이지네이션 기능을 컨트롤러(게시판 뷰)에 반영
    테스트는 'Page' 클래스를 이용하기 때문에
    하는 김에 페이징과 정렬을 함께 테스트
    나중에 정렬 구현하여 이어서 활용한 전망

--- project-board/project-board/src/main/java/com/board/projectboard/controller/ArticleController.java
+++ project-board/project-board/src/main/java/com/board/projectboard/controller/ArticleController.java
@@ -4,7 +4,9 @@ import com.board.projectboard.domain.type.SearchType;
 import com.board.projectboard.dto.response.ArticleResponse;
 import com.board.projectboard.dto.response.ArticleWithCommentsResponse;
 import com.board.projectboard.service.ArticleService;
+import com.board.projectboard.service.PaginationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -23,6 +25,7 @@ import java.util.List;
 public class ArticleController {
 
     private final ArticleService articleService;
+    private final PaginationService paginationService;
     @GetMapping
     public  String articles(
             @RequestParam(required = false) SearchType searchType,
@@ -30,7 +33,13 @@ public class ArticleController {
             @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
             ModelMap map
     ) {
-        map.addAttribute("articles", articleService.searchArticles(searchType, searchvalue, pageable).map(ArticleResponse::from));
+        Page<ArticleResponse> articles = articleService.searchArticles(searchType, searchvalue, pageable).map(ArticleResponse::from);
+        List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
+
+        map.addAttribute("articles", articles);
+        map.addAttribute("paginationBarNumbers", barNumbers);
+        map.addAttribute("searchTypes", SearchType.values());
+
         return "articles/index";
     }
     @GetMapping("/{articleId}")
@@ -38,6 +47,7 @@ public class ArticleController {
         ArticleWithCommentsResponse article = ArticleWithCommentsResponse.from(articleService.getArticle(articleId));
         map.addAttribute("article", article);
         map.addAttribute("articleComments", article.articleCommentsResponse());
+        map.addAttribute("totalCount", articleService.getArticleCount());
         return "articles/detail";
     }
 }
