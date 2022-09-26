84268b4e7193748541451eab0956f85ba99c2784
JEONGBEOM KO <beom9807@gmail.com>
2022-09-26 10:30:59 +0900

    #26 - 테스트 데이터 업데이트 : 유저 추가
    사용자 이름으로 정렬할 때 결과를 눈에 보기 위해 가볍게 테스트 데이터 추가

--- project-board/project-board/src/main/resources/data.sql
+++ project-board/project-board/src/main/resources/data.sql
@@ -1,12 +1,14 @@
 insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at, modified_by) values
 ('Jbk3', '1234', 'Jbk3', 'Jbk3@mail.com', 'I am Jbk3.', now(), 'Jbk3', now(), 'Jbk3')
 ;
+insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at, modified_by) values
+('Jbk2', '1234', 'Jbk2', 'Jbk2@mail.com', 'I am Jbk2.', now(), 'Jbk2', now(), 'Jbk2')
+;
 -- article 123
 insert into article (user_account_user_id, title, content, created_by, modified_by, created_at, modified_at) values
 ('Jbk3', 'Quisque ut erat.', 'Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.
 Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.
-Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.
-#pink', 'Kamilah', 'Murial', '2021-05-30 23:53:46', '2021-03-10 08:48:50'),
+Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.#pink', 'Kamilah', 'Murial', '2021-05-30 23:53:46', '2021-03-10 08:48:50'),
 ('Jbk3', 'Morbi ut odio.', 'Phasellus in felis. Donec semper sapien a libero. Nam dui.
 Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.
 Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.
