5c23842b1ee584eddbae32d435bc180e1d5ce14a
JEONGBEOM KO <beom9807@gmail.com>
2022-10-31 16:29:01 +0900

    #54-스마트에디터를 이용한 이미지 업로드

--- project-board/project-board/src/main/java/com/board/projectboard/controller/ArticleController.java
+++ project-board/project-board/src/main/java/com/board/projectboard/controller/ArticleController.java
@@ -6,7 +6,6 @@ import com.board.projectboard.domain.constant.FormStatus;
 import com.board.projectboard.domain.constant.SearchType;
 import com.board.projectboard.dto.ArticleDto;
 import com.board.projectboard.dto.FileDto;
-import com.board.projectboard.dto.UserAccountDto;
 import com.board.projectboard.dto.request.ArticleRequest;
 import com.board.projectboard.dto.response.ArticleResponse;
 import com.board.projectboard.dto.response.ArticleWithCommentsResponse;
@@ -14,7 +13,6 @@ import com.board.projectboard.dto.security.BoardPrincipal;
 import com.board.projectboard.service.ArticleService;
 import com.board.projectboard.service.FileService;
 import com.board.projectboard.service.PaginationService;
-import io.micrometer.core.instrument.util.StringUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -27,10 +25,10 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.UUID;
 
@@ -45,7 +43,7 @@ public class ArticleController {
 
 
     @GetMapping
-    public  String articles(
+    public String articles(
             @RequestParam(required = false) SearchType searchType,
             @RequestParam(required = false) String searchValue,
             @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
@@ -62,7 +60,7 @@ public class ArticleController {
     }
 
     @GetMapping("/{articleId}")
-    public  String article(
+    public String article(
             @PathVariable Long articleId,
             @RequestParam(required = false) SearchType searchType,
             @RequestParam(required = false) String searchValue,
@@ -86,7 +84,7 @@ public class ArticleController {
             @RequestParam(required = false) String searchValue,
             @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
             ModelMap map
-    ){
+    ) {
         Page<ArticleResponse> articles = articleService.searchArticlesViaHashtag(searchValue, pageable).map(ArticleResponse::from);
         List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
         List<String> hashtags = articleService.getHashtags();
@@ -105,7 +103,7 @@ public class ArticleController {
         return "articles/form";
     }
 
-    @PostMapping ("/form")
+    @PostMapping("/form")
     public String postNewArticle(
             @AuthenticationPrincipal BoardPrincipal boardPrincipal,
             ArticleRequest articleRequest
@@ -133,10 +131,9 @@ public class ArticleController {
             String savePath = System.getProperty("user.dir") + "\\files";
             /* 파일이 저장되는 폴더가 없으면 폴더를 생성합니다. */
             if (!new File(savePath).exists()) {
-                try{
+                try {
                     new File(savePath).mkdir();
-                }
-                catch(Exception e){
+                } catch (Exception e) {
                     e.getStackTrace();
                 }
             }
@@ -151,12 +148,13 @@ public class ArticleController {
             Long fileId = fileService.saveFile(fileDto);
             articleDto.fileId();
             articleService.saveArticle(articleDto);
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return "redirect:/";
     }
-    @PostMapping ("/{articleId}/form")
+
+    @PostMapping("/{articleId}/form")
     public String updateArticle(
             @PathVariable Long articleId,
             @AuthenticationPrincipal BoardPrincipal boardPrincipal,
@@ -168,50 +166,99 @@ public class ArticleController {
         return "redirect:/articles/" + articleId;
     }
 
-    @PostMapping ("/{articleId}/delete")
+    @PostMapping("/{articleId}/delete")
     public String deleteArticle(
             @PathVariable Long articleId,
             @AuthenticationPrincipal BoardPrincipal boardPrincipal
-            ) {
+    ) {
         articleService.deleteArticle(articleId, boardPrincipal.getUsername());
 
         return "redirect:/articles";
     }
-    @RequestMapping(value="/singleImageUploader.do")
+
+    @RequestMapping(value = "/singleImageUploader.do")
     public String simpleImageUploader(
-            HttpServletRequest req, SmarteditorVO smarteditorVO, @PathVariable Long articleId)
-            throws UnsupportedEncodingException{
+            HttpServletRequest request, SmarteditorVO smarteditorVO) throws UnsupportedEncodingException {
         String callback = smarteditorVO.getCallback();
         String callback_func = smarteditorVO.getCallback_func();
         String file_result = "";
-        String result ="";
-        MultipartFile multiFile = smarteditorVO.getFiledata();
         try {
-            if(multiFile != null && multiFile.getSize()>0 &&
-                    StringUtils.isNotBlank(multiFile.getName())) {
-                if (multiFile.getContentType().toLowerCase().startsWith("image/")) {
-                    String oriName = multiFile.getName();
-                    String uploadPath = req.getServletContext().getRealPath("/img");
-                    String path = uploadPath + "/smarteditor/";
-                    File file = new File(path);
-                    if (!file.exists()) {
-                        file.mkdirs();
-                    }
-                    String fileName = UUID.randomUUID().toString();
-                    smarteditorVO.getFiledata().transferTo(new File(path + fileName));
-                    file_result += "&bNewLine=true&sFileName=" + oriName +
-                            "&sFileURL=/img/smarteditor/" + fileName;
-                } else {
-                    file_result += "#errst=error";
+            if (smarteditorVO.getFiledata() != null && smarteditorVO.getFiledata().getOriginalFilename() != null && !smarteditorVO.getFiledata().getOriginalFilename().equals("")) {
+                //파일이 존재하면
+                String original_name = smarteditorVO.getFiledata().getOriginalFilename();
+                String ext = original_name.substring(original_name.lastIndexOf(".") + 1);
+                //파일 기본경로
+                String defaultPath = request.getSession().getServletContext().getRealPath("/");
+                //파일 기본경로_상세경로
+                String path = defaultPath + "resources" + File.separator + "upload" + File.separator;
+                File file = new File(path);
+                if (!file.exists()) {
+                    file.mkdirs();
                 }
-            }else{
-                    file_result += "#errst=error";
-                }
+                //서버에 업로드할 파일명(한글문제로 인해 원본파일은 올리지 않는 것이 좋음
+                String realname = UUID.randomUUID().toString() + "." + ext;
+                //서버에 파일쓰기
+                smarteditorVO.getFiledata().transferTo(new File(path + realname));
+                file_result += "&bNewLine=true&sFileName=" + original_name + "&sFileURL=/resources/upload/" + realname;
+            } else {
+                file_result += "&errstr=error";
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        return "redirect:/articles/" + articleId;
+        return "redirect:" + callback + "?callback_func=" + URLEncoder.encode(callback_func, "UTF-8") + file_result;
     }
 
+    @RequestMapping(value = "/multiplePhotoUpload.do")
+    public void multiplePhotoUpload(
+            HttpServletRequest request, HttpServletResponse response) {
+        try {
+            //파일정보
+            String sFileInfo = "";
+            //파일명을 받는다 - 일반 원본파일명
+            String filename = request.getHeader("file-name");
+            //파일 확장자
+            String filename_ext = filename.substring(filename.lastIndexOf(".") + 1);
+            //확장자를 소문자로 변경
+            filename_ext = filename_ext.toLowerCase();
+            //파일 기본경로
+            String dftFilePath = request.getSession().getServletContext().getRealPath("/");
+            //파일 기본경로_상세경로
+            String filePath = dftFilePath + "resources" + File.separator + "upload" + File.separator;
+            File file = new File(filePath);
+            if (!file.exists()) {
+                file.mkdirs();
+            }
+            String realFileNm = "";
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+            String today = formatter.format(new java.util.Date());
+            realFileNm = today + UUID.randomUUID().toString() + filename.substring(filename.lastIndexOf("."));
+            String rlFileNm = filePath + realFileNm;
+
+            InputStream is = request.getInputStream();
+            OutputStream os = new FileOutputStream(rlFileNm);
+            int numRead;
+            byte b[] = new byte[Integer.parseInt(request.getHeader("file-size"))];
+            while ((numRead = is.read(b, 0, b.length)) != -1) {
+                os.write(b, 0, numRead);
+            }
+            if (is != null) {
+                is.close();
+            }
+            os.flush();
+            os.close();
+
+            //서버에 파일쓰기
+            //정보출력
+            sFileInfo += "&bNewLine=true";
+            sFileInfo += "&sFileName=" + filename;
+            PrintWriter print = response.getWriter();
+            print.print(sFileInfo);
+            print.flush();
+            print.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
